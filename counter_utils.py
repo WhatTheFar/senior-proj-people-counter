b071155670e138f4b3d90134623d7e0d59edd671
WhatTheFar <jakpat.m@gmail.com>
2019-10-04 13:39:56 +0700

    refactor(utils): snake_case variable names

--- counter_utils.py
+++ counter_utils.py
@@ -6,87 +6,87 @@ import time
 
 
 def start_simple_counter(video, debug, output):
-    def CheckLineCrossing(centerMove, CoorExitLine1, CoorExitLine2):
-        (x, y) = centerMove
+    def check_line_crossing(center_move, coor_exit_line1, coor_exit_line2):
+        (x, y) = center_move
         # outside
-        if (y > CoorExitLine1[1]) and (x >= CoorExitLine1[0]):
+        if (y > coor_exit_line1[1]) and (x >= coor_exit_line1[0]):
             return True
         # inside
         else:
             return False
 
-    def autoPop(arr, lenght):
-        if len(arr) > lenght:
+    def auto_pop(arr, length):
+        if len(arr) > length:
             arr.pop(0)
 
-    def definePoint12(rec):
+    def define_point12(rec):
         return (rec[0], rec[1]), (rec[2] + rec[0], rec[3] + rec[1])
 
-    def setCenter(box):
-        return (int(box[0] + box[2] / 2), int(box[1] + box[3] / 2))
+    def set_center(box):
+        return int(box[0] + box[2] / 2), int(box[1] + box[3] / 2)
 
-    def findDistance(rec1, rec2):
+    def find_distance(rec1, rec2):
         distance = 10000
         if rec1 is not None and rec2 is not None:
-            centerRec1 = setCenter(rec1)
-            centerRec2 = setCenter(rec2)
-            diffMinX = abs(centerRec1[0] - centerRec2[0])
-            diffMinY = abs(centerRec1[1] - centerRec2[1])
-            distance = (pow(diffMinX, 2) + pow(diffMinY, 2)) ** (1 / 2)
+            center_rec1 = set_center(rec1)
+            center_rec2 = set_center(rec2)
+            diff_min_x = abs(center_rec1[0] - center_rec2[0])
+            diff_min_y = abs(center_rec1[1] - center_rec2[1])
+            distance = (pow(diff_min_x, 2) + pow(diff_min_y, 2)) ** (1 / 2)
         return distance
 
-    def findTrack(bbox, focus):
+    def find_track(bbox, focus):
         index = -1
-        if pointMove != []:
+        if point_move:
             # set dif of last
-            last = pointMove[len(pointMove) - 1][len(pointMove[len(pointMove) - 1]) - 1]
-            distanceMin = findDistance(last, bbox)
-            index = len(pointMove) - 1
-            for i in range(len(pointMove) - 1):
-                rec = pointMove[i][len(pointMove[i]) - 1]
-                distance = findDistance(rec, bbox)
-                if distance < distanceMin:
+            last = point_move[len(point_move) - 1][len(point_move[len(point_move) - 1]) - 1]
+            distance_min = find_distance(last, bbox)
+            index = len(point_move) - 1
+            for i in range(len(point_move) - 1):
+                rec = point_move[i][len(point_move[i]) - 1]
+                distance = find_distance(rec, bbox)
+                if distance < distance_min:
                     index = i
-                    distanceMin = distance
-            rec = pointMove[index][len(pointMove[index]) - 1]
-            pdistance = findDistance(focus, bbox)
+                    distance_min = distance
+            rec = point_move[index][len(point_move[index]) - 1]
+            pdistance = find_distance(focus, bbox)
 
-            distanceMax = int(0.003 * pdistance ** (2))
+            distance_max = int(0.003 * pdistance ** (2))
             if rec[2] * rec[3] > bbox[2] * bbox[3]:
                 max = ((pow(rec[2], 2) + pow(rec[3], 2)) ** (1 / 2)) / 2
             else:
                 max = ((pow(bbox[2], 2) + pow(bbox[3], 2)) ** (1 / 2)) / 2
-            if distanceMin >= distanceMax + max:
+            if distance_min >= distance_max + max:
                 index = -1
         return index
 
-    def TrackMove(rec, pfocus):
-        nonlocal pointMove, firstTrack, statusMove
+    def track_move(rec, pfocus):
+        nonlocal point_move, first_track, status_move
         # find track
-        indexTrack = findTrack(rec, pfocus)
-        p1, p2 = definePoint12(rec)
+        index_track = find_track(rec, pfocus)
+        p1, p2 = define_point12(rec)
         # set new track
-        if indexTrack == -1:
-            indexTrack = len(firstTrack)
-            firstTrack.append(rec)
-            pointMove.append([])
-            pointMove[indexTrack].append(rec)
-            statusMove.append(30)
+        if index_track == -1:
+            index_track = len(first_track)
+            first_track.append(rec)
+            point_move.append([])
+            point_move[index_track].append(rec)
+            status_move.append(30)
         else:
-            pointMove[indexTrack].append(rec)
-            statusMove[indexTrack] = 30
-        autoPop(pointMove[indexTrack], 2)
-        return indexTrack
+            point_move[index_track].append(rec)
+            status_move[index_track] = 30
+        auto_pop(point_move[index_track], 2)
+        return index_track
 
-    def popIndex(firstTrack, pointMove, statusMove, i):
-        firstTrack.pop(i)
-        pointMove.pop(i)
-        statusMove.pop(i)
+    def pop_index(first_track, point_move, status_move, i):
+        first_track.pop(i)
+        point_move.pop(i)
+        status_move.pop(i)
 
-    def checkmark(Frame, frameSize, sizeLong, OffsetY, OffsetX):
+    def check_mark(frame, frame_size, size_long, offset_y, offset_x):
         # load the Frame
         # frameSize1 = (600, 450)
-        areaFrame = frameSize[0] * frameSize[1]
+        area_frame = frame_size[0] * frame_size[1]
 
         # define the list of boundaries
         boundary = ([40, 150, 30], [80, 210, 160])
@@ -99,9 +99,9 @@ def start_simple_counter(video, debug, output):
 
         # find the colors within the specified boundaries and apply
         # the mask
-        image_hsv = cv2.cvtColor(Frame, cv2.COLOR_BGR2HSV)
+        image_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(image_hsv, lower, upper)
-        output = cv2.bitwise_and(Frame, Frame, mask=mask)
+        output = cv2.bitwise_and(frame, frame, mask=mask)
         # Dilate image and find all the contours
         cnts = cv2.findContours(
             mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -109,78 +109,77 @@ def start_simple_counter(video, debug, output):
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
         for cnt in cnts:
             Area = cv2.contourArea(cnt)
-            if Area < areaFrame * 0.00037 or Area > areaFrame * 0.00074:
+            if Area < area_frame * 0.00037 or Area > area_frame * 0.00074:
                 continue
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(output, (x, y), (x + w, y + h), (0, 0, 255), 2)
             if w > h * 1.3:
-                marks.append(setCenter((x, y, w, h)))
+                marks.append(set_center((x, y, w, h)))
         if len(marks) >= 2:
             if marks[1][0] < marks[0][0]:
                 tmp = marks[0]
                 marks[0] = marks[1]
                 marks[1] = tmp
-            sizeLong = int(abs(marks[0][0] - marks[1][0]) / 2)
-            OffsetY = int(frameSize[1] - (marks[0][1] + marks[1][1]) / 2)
-            OffsetX = int(frameSize[0] / 2 - (marks[0][0] + sizeLong))
+            size_long = int(abs(marks[0][0] - marks[1][0]) / 2)
+            offset_y = int(frame_size[1] - (marks[0][1] + marks[1][1]) / 2)
+            offset_x = int(frame_size[0] / 2 - (marks[0][0] + size_long))
             # break
-        return sizeLong, OffsetY, OffsetX
+        return size_long, offset_y, offset_x
 
     # Global variables
     # initialize the frame dimensions (we'll set them as soon as we read
     # the first frame from the video)
-    W = None
-    H = None
-    frameSize = (600, 800)
-    areaFrame = None
-    MinContourArea = None
-    MaxContourArea = None
+    w = None
+    h = None
+    frame_size = (600, 800)
+    area_frame = None
+    min_contour_area = None
+    max_contour_area = None
 
     # line's position
-    OffsetY = 0
-    OffsetX = 0
-    sizeLong = 0
+    offset_y = 0
+    offset_x = 0
+    size_long = 0
     padding = 0
     margin = 0
 
-    def setInitialVariable(w, h):
-        nonlocal W, H, frameSize, areaFrame, MinContourArea, MaxContourArea, OffsetX, OffsetY, sizeLong, padding, margin
+    def set_initial_variable(new_w, new_h):
+        nonlocal w, h, frame_size, area_frame, min_contour_area, max_contour_area, \
+            offset_x, offset_y, size_long, padding, margin
         # Set initial frame size.
-        W = w
-        H = h
-        frameSize = (w, h)
-        areaFrame = frameSize[0] * frameSize[1]
+        w = new_w
+        h = new_h
+        frame_size = (w, h)
+        area_frame = frame_size[0] * frame_size[1]
 
         # minimum size 0.01%/maximum size 0.1%
-        MinContourArea = areaFrame * 0.05
-        MaxContourArea = areaFrame * 0.5
+        min_contour_area = area_frame * 0.05
+        max_contour_area = area_frame * 0.5
 
         # line's position
-        OffsetY = int(frameSize[1] * 0.6)
+        offset_y = int(frame_size[1] * 0.6)
         # OffsetY = 0
-        OffsetX = -30
-        sizeLong = int(frameSize[0] * 0.4)
-        # padding = frameSize[1] * 0.02
-        margin = frameSize[1] * 0.03
+        offset_x = -30
+        size_long = int(frame_size[0] * 0.4)
+        # padding = frame_size[1] * 0.02
+        margin = frame_size[1] * 0.03
 
     # tracking variable
-    pointMove = []
-    firstTrack = []
-    statusMove = []
+    point_move = []
+    first_track = []
+    status_move = []
 
     # define counting number of people
-    countPeople = 0
+    people_count = 0
 
     idle_time = 0
 
     # object for BackgroundSubtractor
-    fgbg = cv2.createBackgroundSubtractorMOG2(
-        history=2000, varThreshold=100, detectShadows=False)
+    fgbg = cv2.createBackgroundSubtractorMOG2(history=2000, varThreshold=100, detectShadows=False)
 
     # Initialize mutithreading the video stream.
     if video is None:
-        camera = VideoStream(src=0, usePiCamera=True,
-                             resolution=frameSize, framerate=32).start()
+        camera = VideoStream(src=0, usePiCamera=True, resolution=frame_size, framerate=32).start()
     else:
         camera = cv2.VideoCapture(video)
 
@@ -196,31 +195,31 @@ def start_simple_counter(video, debug, output):
     while True:
         # If using a webcam instead of the Pi Camera,
         if video is None:
-            Frame = camera.read()
+            frame = camera.read()
         # If using a video file
         else:
-            _, Frame = camera.read()
+            _, frame = camera.read()
             # cannot fetch Frame
-            if Frame is None:
+            if frame is None:
                 break
-            Frame = imutils.resize(Frame, width=500)
+            frame = imutils.resize(frame, width=500)
 
         # cannot fetch Frame
-        if Frame is None:
+        if frame is None:
             break
 
         # if the frame dimensions are empty, set them
-        if W is None or H is None:
-            (h, w) = Frame.shape[:2]
-            setInitialVariable(w, h)
+        if w is None or h is None:
+            (h, w) = frame.shape[:2]
+            set_initial_variable(w, h)
 
         # if we are supposed to be writing a video to disk, initialize the writer
         if output is not None and writer is None:
             fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-            writer = cv2.VideoWriter(output, fourcc, 30, (W, H), True)
+            writer = cv2.VideoWriter(output, fourcc, 30, (w, h), True)
 
-        # gray-scale convertion and Gaussian blur filter applying
-        gray = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+        # gray-scale conversion and Gaussian blur filter applying
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         ret, gray = cv2.threshold(gray, 40, 100, cv2.THRESH_BINARY)
         if idle_time < 2:
             idle_time += 1
@@ -239,66 +238,67 @@ def start_simple_counter(video, debug, output):
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
         # plot reference lines (entrance and exit lines)
-        centerLine = (int(frameSize[0] / 2 - OffsetX), int(frameSize[1] - OffsetY))
-        CoorExitLine1 = (centerLine[0] - sizeLong, centerLine[1])
-        CoorExitLine2 = (centerLine[0] + sizeLong, centerLine[1])
-        cv2.line(Frame, CoorExitLine1, CoorExitLine2, (0, 0, 255), 2)
-        Line = (CoorExitLine1[0], CoorExitLine1[1], CoorExitLine2[0] -
-                CoorExitLine1[0], CoorExitLine2[1] - CoorExitLine2[1])
+        center_line = (int(frame_size[0] / 2 - offset_x), int(frame_size[1] - offset_y))
+        coor_exit_line1 = (center_line[0] - size_long, center_line[1])
+        coor_exit_line2 = (center_line[0] + size_long, center_line[1])
+        cv2.line(frame, coor_exit_line1, coor_exit_line2, (0, 0, 255), 2)
+        line = (coor_exit_line1[0], coor_exit_line1[1], coor_exit_line2[0] -
+                coor_exit_line1[0], coor_exit_line2[1] - coor_exit_line2[1])
 
         # check all found countours
         for c in cnts:
-            Area = cv2.contourArea(c)
+            area = cv2.contourArea(c)
             rec = cv2.boundingRect(c)
-            p1, p2 = definePoint12(rec)
-            cv2.rectangle(Frame, p1, p2, (0, 255, 200), 2)
-            if Area > MaxContourArea:
+            p1, p2 = define_point12(rec)
+            cv2.rectangle(frame, p1, p2, (0, 255, 200), 2)
+            if area > max_contour_area:
                 continue
-            elif Area < MinContourArea:
+            elif area < min_contour_area:
                 break
 
             # find object's centroid
-            ObjectCentroid = setCenter(rec)
-            indexTrack = TrackMove(rec, Line)
+            object_centroid = set_center(rec)
+            index_track = track_move(rec, line)
 
-            (x, y, w, h) = firstTrack[indexTrack]
-            colorB = 0
-            colorR = 0
-            firstCenter = setCenter(firstTrack[indexTrack])
+            (x, y, w, h) = first_track[index_track]
+            color_b = 0
+            color_r = 0
+            first_center = set_center(first_track[index_track])
 
-            if CheckLineCrossing(firstCenter, CoorExitLine1, CoorExitLine2):
-                colorR = 255
+            if check_line_crossing(first_center, coor_exit_line1, coor_exit_line2):
+                color_r = 255
             else:
-                colorB = 255
+                color_b = 255
 
-            cv2.rectangle(Frame, p1, p2, (colorB, int(
-                (indexTrack + 1) * 70), colorR), 2)
+            cv2.rectangle(frame, p1, p2, (color_b, int(
+                (index_track + 1) * 70), color_r), 2)
 
-            if abs(firstCenter[1] - CoorExitLine1[1]) > margin and abs(ObjectCentroid[1] - CoorExitLine1[1]) > margin:
-                if not CheckLineCrossing(firstCenter, CoorExitLine1, CoorExitLine2) and CheckLineCrossing(
-                        ObjectCentroid,
-                        CoorExitLine1,
-                        CoorExitLine2):
-                    countPeople -= 1
-                    firstTrack[indexTrack] = rec
-                elif not CheckLineCrossing(ObjectCentroid, CoorExitLine1, CoorExitLine2) and CheckLineCrossing(
-                        firstCenter,
-                        CoorExitLine1,
-                        CoorExitLine2):
-                    countPeople += 1
-                    firstTrack[indexTrack] = rec
-        for i in range(len(statusMove)):
-            if statusMove[i] <= 0:
-                popIndex(firstTrack, pointMove, statusMove, i)
+            if abs(first_center[1] - coor_exit_line1[1]) > margin and abs(
+                    object_centroid[1] - coor_exit_line1[1]) > margin:
+                if not check_line_crossing(first_center, coor_exit_line1, coor_exit_line2) and check_line_crossing(
+                        object_centroid,
+                        coor_exit_line1,
+                        coor_exit_line2):
+                    people_count -= 1
+                    first_track[index_track] = rec
+                elif not check_line_crossing(object_centroid, coor_exit_line1, coor_exit_line2) and check_line_crossing(
+                        first_center,
+                        coor_exit_line1,
+                        coor_exit_line2):
+                    people_count += 1
+                    first_track[index_track] = rec
+        for i in range(len(status_move)):
+            if status_move[i] <= 0:
+                pop_index(first_track, point_move, status_move, i)
                 break
 
         # Write entrance and exit counter values on frame and shows it
-        cv2.putText(Frame, "count: {}".format(str(countPeople)), (10, 50),
+        cv2.putText(frame, "count: {}".format(str(people_count)), (10, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
 
         # check to see if we should write the frame to disk
         if writer is not None:
-            writer.write(Frame)
+            writer.write(frame)
 
         if debug:
             cv2.imshow("Grey", gray)
@@ -309,7 +309,7 @@ def start_simple_counter(video, debug, output):
             cv2.imshow("Mask", fgmask)
             cv2.moveWindow('Mask', 719, 315)
 
-            cv2.imshow("Original Frame", Frame)
+            cv2.imshow("Original Frame", frame)
 
             # Don't wait for key
             # if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -320,8 +320,8 @@ def start_simple_counter(video, debug, output):
             if k == ord('q'):
                 break
 
-        for i in range(len(statusMove)):
-            statusMove[i] -= 1
+        for i in range(len(status_move)):
+            status_move[i] -= 1
         idle_time += 1
 
     # cleanup the camera and close any open windows
